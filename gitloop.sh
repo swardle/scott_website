57fbc16d52a60b976ffbaee68fb0904b4859d731
Scott Wardle <swardle@gmail.com>
2020-05-06 21:16:01 -0600

    One more gitloop

--- gitloop.sh
+++ gitloop.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 while true
+
+	git fetch;
+	LOCAL=$(git rev-parse HEAD);
+	REMOTE=$(git rev-parse @{u});
+
+	#if our local revision id doesn't match the remote, we will need to pull the changes
+	if [ $LOCAL != $REMOTE ]; then
+		#pull and merge changes
+		git pull origin master;
+		mv ~/public_html/scott_website ~/public_html/sweb
+	fi
+	sleep 60
+
 do
-
-git fetch;
-LOCAL=$(git rev-parse HEAD);
-REMOTE=$(git rev-parse @{u});
-
-#if our local revision id doesn't match the remote, we will need to pull the changes
-if [ $LOCAL != $REMOTE ]; then
-    #pull and merge changes
-    git pull origin master;
-    mv ~/public_html/scott_website ~/public_html/sweb
-fi
-sleep 60
-done
