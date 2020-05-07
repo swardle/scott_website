6e45d8153d3a0c5cc489a9fb84ca02a5986c8883
Scott Wardle <swardle@gmail.com>
2020-05-06 21:28:10 -0600

    Hack to see gitloop

--- gitloop.sh
+++ gitloop.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-while true do
+while true
+do
+	echo hi there
 	git fetch;
 	LOCAL=$(git rev-parse HEAD);
 	REMOTE=$(git rev-parse @{u});
+	echo hi there2 $LOCAL != $REMOTE
 
 	#if our local revision id doesn't match the remote, we will need to pull the changes
 	if [ $LOCAL != $REMOTE ]; then
 		#pull and merge changes
 		git pull origin master;
 		mv ~/public_html/scott_website ~/public_html/sweb
+		echo hi there3
 	fi
-	sleep 60
+	sleep 5
+	echo hi there4
 done
