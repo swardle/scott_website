3c7bdbc1638e8041819dc4228cedefa98e7dbe89
Scott Wardle <swardle@gmail.com>
2020-05-10 15:51:29 -0700

    update gitloop

--- gitcall.sh
+++ gitcall.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-cd ~/public_html/scott_website/
+cd 	/home2/swardlec/repositories/scott_website/
 git fetch;
 LOCAL=$(git rev-parse HEAD);
 REMOTE=$(git rev-parse @{u});
-echo looking for change $LOCAL != $REMOTE $now >> ~/gitloop.txt
+echo looking for change $LOCAL != $REMOTE $now >> /home2/swardlec/gitloop.txt
 
 #if our local revision id doesn't match the remote, we will need to pull the changes
 if [ $LOCAL != $REMOTE ]; then
-    echo found a difference >> ~/gitloop.txt
+    echo found a difference >> /home2/swardlec/gitloop.txt
     #pull and merge changes
     git pull origin master;
-    rsync -a ~/public_html/scott_website/ ~/public_html/sweb/
+    rsync -a /home2/swardlec/repositories/scott_website/ /home2/swardlec/public_html/sweb/
 fi
 sleep 60
